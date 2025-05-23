206060bed373ddd0d1ede64bc423980dc39112e3
neosome <daniildaresin@gmail.com>
2025-05-23 23:32:19 +0300

    init start

---
+++ src/main/java/com/example/demo/calculator/DemoCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCalculatorApplication.class, args);
+	}
+
+}
